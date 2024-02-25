abaf7c55261d3d69f0e0ee558504712afaf76c66
Vinícius Rios <129979492+ViniiRios@users.noreply.github.com>
2024-02-25 19:48:28 -0300

    Add files via upload

---
+++ Exercício 2 - Lista 1/Pessoa.java
@@ -0,0 +1,9 @@
+public class Pessoa {
+
+    public String cpf;
+    public String nome;
+    public int idade;
+    public double peso;
+    public double altura;
+
+}
---
+++ Exercício 2 - Lista 1/ProgramaPessoa.java
@@ -0,0 +1,10 @@
+public class ProgramaPessoa {
+    public static void main (String[] args) {
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.nome = "Vinícius Rios";
+        System.out.println("Nome da Pessoa: " + pessoa.nome);
+
+    }
+    
+}
