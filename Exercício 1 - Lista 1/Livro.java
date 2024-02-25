92f20fd4c955a702fa0a970033686b156311a264
Vinícius Rios <129979492+ViniiRios@users.noreply.github.com>
2024-02-25 19:48:13 -0300

    Add files via upload

---
+++ Exercício 1 - Lista 1/Livro.java
@@ -0,0 +1,20 @@
+public class Livro {
+    private String titulo;
+    private String autor;
+    private String assunto;
+    private String editora;
+    private String ISBN;
+    private int qtdPaginas;
+
+    public String getTitulo(){
+        return titulo;
+    }
+
+
+    public void setTitulo(String titulo) {
+
+        this.titulo = titulo;
+
+    }
+
+}
---
+++ Exercício 1 - Lista 1/ProgramaLivro.java
@@ -0,0 +1,7 @@
+public class ProgramaLivro {
+    public static void main(String[] args) {
+        Livro livro = new Livro();
+        livro.setTitulo("Red, White & Royal Blue");
+        System.out.println(livro.getTitulo());
+    }
+}
